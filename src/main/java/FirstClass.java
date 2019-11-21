b418dd328f9c41002580e9fd8cd728b5c8d2d8a0
nazife <nazifekorkut@gmail.com>
2019-11-21 17:06:58 -0500

    new hiii

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hi world");
+        System.out.println("i m working");
+        System.out.println("At school");
+
+
     }
 }
 
---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,6 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("this is second class");
+        System.out.println("in second change");
+    }
+}
