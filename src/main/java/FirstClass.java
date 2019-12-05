25351c395e5b83a7e5a1f6d8404a45a3982890ae
nazifek <55412264+nazifek@users.noreply.github.com>
2019-12-05 15:55:35 -0500

    Update FirstClass.java

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -3,7 +3,8 @@ public class FirstClass {
         System.out.println("Hi world");
         System.out.println("i m working");
         System.out.println("At school");
-
+System.out.println("i m working");
+        System.out.println("At home");
 
         //hi
 
